449aa2a663b319f85bcb8b48277675f22438bfdf
ChhitizeKumar <chhitizeathghara11@gmail.com>
2023-11-29 01:59:55 +0530

    Display Buttom

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,20 +3,13 @@ import "./NavBar.css";
 import { IoFilterOutline } from "react-icons/io5";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
-function useClickOutside(ref, ref2, onClickOutside) {
+function useClickOutside(ref, onClickOutside) {
   useEffect(() => {
     /**
      * Invoke Function onClick outside of element
      */
     function handleClickOutside(event) {
-      // console.log(ref2.current.contains(event.target));
-      // console.log(event.target);
-      if (
-        ref.current &&
-        !ref.current.contains(event.target) &&
-        !ref2.current.contains(event.target)
-      ) {
-        // console.log(ref.current.contains(event.target))
+      if (ref.current && !ref.current.contains(event.target)) {
         onClickOutside();
       }
     }
@@ -26,67 +19,72 @@ function useClickOutside(ref, ref2, onClickOutside) {
       // dispose
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [ref, ref2, onClickOutside]);
+  }, [ref, onClickOutside]);
 }
 
 const NavBar = ({ filter, setFilter }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef(null);
-  const ref2 = useRef(null);
-
 
   const handleSelect = (e) => {
     e.preventDefault();
     const newFilter = { ...filter, [e.target.name]: e.target.value };
-    localStorage.setItem("filter",JSON.stringify(newFilter))
+    localStorage.setItem("filter", JSON.stringify(newFilter));
     setFilter(newFilter);
   };
 
-  useClickOutside(ref, ref2, () => {
+  useClickOutside(ref, () => {
     setOpen(false);
   });
 
   return (
     <div className="displayed">
-      <button className="dropdown" onClick={() => setOpen(!open)} ref={ref2}>
-        <div className="display-icon">
-          <IoFilterOutline />
-        </div>
-        Display
-        <div className="filter-icon">
-          <RiArrowDropDownLine fontSize={"15px"} />
-        </div>
-      </button>
+      <div ref={ref} className="wrapper">
+        <button
+          className="dropdown"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <div className="display-icon">
+            <IoFilterOutline />
+          </div>
+          Display
+          <div className="filter-icon">
+            <RiArrowDropDownLine fontSize={"15px"} />
+          </div>
+        </button>
 
-      {open && (
-        <div className="filter" ref={ref}>
-          <label>
-            Grouping
-            <select
-              name="groupBy"
-              id=""
-              onChange={handleSelect}
-              defaultValue={filter.groupBy}
-            >
-              <option value="status">Status</option>
-              <option value="priority">Priority</option>
-              <option value="users">Users</option>
-            </select>
-          </label>
-          <label>
-            Ordering
-            <select
-              name="orderBy"
-              id=""
-              onChange={handleSelect}
-              defaultValue={filter.orderBy}
-            >
-              <option value="priority">Priority</option>
-              <option value="title">Title</option>
-            </select>
-          </label>
-        </div>
-      )}
+        {open && (
+          <div className="filter">
+            <label>
+              Grouping
+              <select
+                name="groupBy"
+                id=""
+                onChange={handleSelect}
+                defaultValue={filter.groupBy}
+              >
+                <option value="status">Status</option>
+                <option value="priority">Priority</option>
+                <option value="users">Users</option>
+              </select>
+            </label>
+            <label>
+              Ordering
+              <select
+                name="orderBy"
+                id=""
+                onChange={handleSelect}
+                defaultValue={filter.orderBy}
+              >
+                <option value="priority">Priority</option>
+                <option value="title">Title</option>
+              </select>
+            </label>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
